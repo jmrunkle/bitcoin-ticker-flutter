690ea7783a722d24d009e200f7aa9c37b68e22ec
jmrunkle <jmrunkle@google.com>
2020-05-10 07:30:55 -0700

    Final version of crypto app.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,40 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  static const String apiKey = '25195643-B238-479F-9D38-C298D0BF571E';
+  static const String urlBase = 'https://rest-sandbox.coinapi.io';
+
+  Future<double> getCoinData({String crypto, String currency}) async {
+    http.Response response =
+        await http.get('$urlBase/v1/exchangerate/$crypto/$currency', headers: {
+      'X-CoinAPI-Key': apiKey,
+      'Accept': 'application/json',
+      'Accept-Encoding': 'deflate, gzip',
+    });
+
+    if (response.statusCode == 200) {
+      String content = extractContent(response);
+      return json.decode(content)['rate'];
+    } else {
+      print('Error getting exchange rate: '
+          'statusCode=${response.statusCode} reason=${response.reasonPhrase}');
+      return null;
+    }
+  }
+
+  String extractContent(http.Response response) {
+    List<int> decompressedBytes;
+    switch (response.headers['Content-Encoding']) {
+      case 'deflate':
+        decompressedBytes = zlib.decode(response.bodyBytes);
+        break;
+      case 'gzip':
+        decompressedBytes = gzip.decode(response.bodyBytes);
+        break;
+      default:
+        return response.body;
+    }
+    return utf8.decode(decompressedBytes);
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +12,36 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  Map<String, String> ratesByCrypto = {};
+  CoinData coinData = CoinData();
+
+  @override
+  void initState() {
+    super.initState();
+    updateRates();
+  }
+
+  void initRates() {
+    cryptoList.forEach((crypto) => ratesByCrypto[crypto] = '?');
+  }
+
+  void updateRates() {
+    initRates();
+    cryptoList.forEach(updateRate);
+  }
+
+  void updateRate(String crypto) async {
+    double rate = await coinData.getCoinData(
+      crypto: crypto,
+      currency: selectedCurrency,
+    );
+    String rateAsString = rate == null ? '?' : rate.truncate().toString();
+    setState(() {
+      ratesByCrypto[crypto] = rateAsString;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,36 +52,83 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: cryptoList.map(cryptoCard).toList(),
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getIosPicker() : getDefaultPicker(),
           ),
         ],
       ),
     );
   }
+
+  Widget cryptoCard(String crypto) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $crypto = ${ratesByCrypto[crypto]} $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // To prevent a flood of calls when using the cupertino picker, we need
+  // to use a timer to make sure we only send one after the user has settled
+  // on a particular value for at least 2 seconds
+  Timer cupertinoPickerTimer;
+
+  Widget getIosPicker() => CupertinoPicker(
+        backgroundColor: Colors.lightBlue,
+        itemExtent: 40.0,
+        children: currenciesList.map(makeIosCurrency).toList(),
+        onSelectedItemChanged: (index) {
+          if (cupertinoPickerTimer != null) {
+            cupertinoPickerTimer.cancel();
+          }
+          cupertinoPickerTimer = Timer(Duration(seconds: 2), () {
+            setState(() {
+              selectedCurrency = currenciesList[index];
+              updateRates();
+            });
+            cupertinoPickerTimer = null;
+          });
+        },
+      );
+
+  Widget makeIosCurrency(String currency) => Text(currency);
+
+  Widget getDefaultPicker() => DropdownButton<String>(
+        value: selectedCurrency,
+        items: currenciesList.map(makeCurrencyItem).toList(),
+        onChanged: (value) => setState(() {
+          selectedCurrency = value;
+          updateRates();
+        }),
+      );
+
+  DropdownMenuItem<String> makeCurrencyItem(String currency) =>
+      DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      );
 }
